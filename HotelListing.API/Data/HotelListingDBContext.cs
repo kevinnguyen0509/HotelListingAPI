90523bec2df937eedbee3aa63f7f46378833dec2
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2023-11-09 11:32:14 -0800

    Set up first two tables using entity framework

---
+++ HotelListing.API/Data/HotelListingDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDBContext : DbContext
+    {
+        public HotelListingDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
