d8277de4ee88eb18243ad718ae12c491e700fe81
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2023-12-07 20:38:28 -0800

    Refactored code and included automapper

---
+++ HotelListing.API/Models/Country/CountryDTO.cs
@@ -0,0 +1,12 @@
+﻿using HotelListing.API.Models.Hotel;
+
+namespace HotelListing.API.Models.Country
+{
+    public class CountryDTO : BaseCountryDTO
+    {
+        public int Id { get; set; }
+
+        public List<HotelDTO> Hotels { get; set; }
+
+    }
+}
---
+++ HotelListing.API/Repository/CountriesRepository.cs
@@ -0,0 +1,23 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class CountriesRepository : GenericRepository<Country>, ICountriesRepository
+    {
+        private readonly HotelListingDBContext _context;
+        public CountriesRepository(HotelListingDBContext context) : base(context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<Country> GetDetails(int id)
+        {
+            return await _context.Countries.Include(q => q.Hotels)
+                .FirstOrDefaultAsync(db => db.Id == id);
+        }
+    }
+}
